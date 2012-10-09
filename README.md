38f0b9d5dd5db2243632cfbcd585a8e0cc58cfbc
douglas <dougamanzelmann@gmail.com>
2012-10-09 13:02:59 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is the second version of my A/V Schedule App.
+
+What's different?
+- One thing, really: The first version was my first app. I hadn't learned testing yet; I just wanted to make an app.
+- So I'll design this version with TDD in mind.
+
+Where's the first version? [A/V Schedule v1 is here on Github](https://github.com/douglasmanzelmann/avschedule).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avsv2::Application.config.secret_token = '47ad6db8c679ea362b2c96fa0039d641051aadc224ebd73d24da518c03d490d924014d24567e71ead3a557b18bb9517462c1195094f9b7403c61b79e72bb3b36'
