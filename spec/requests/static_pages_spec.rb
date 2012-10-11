7a769009d0cb6984f4528cbea36fac6d62add640
douglas <dougamanzelmann@gmail.com>
2012-10-10 21:04:58 -0400

    added some #home integration tests on static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-	describe ".home" do 
+	describe "#home" do 
 		before { visit root_path }
 
-		it "displays the correct h1 text" do 
-		  page.should have_selector('h1', text: 'A/V Schedule')
+		it "displays A/V Schedule Branding in <header>" do 
+		  page.find('header').should have_selector('a', text: 'A/V Schedule')
 		end
 
-    it "displays the current date"		
+    it "displays the current date" do 
+      page.should have_selector('h1', text: "#{Date.today.strftime('%A, %B %d, %Y')}")
+    end
+
+    it "displays a link to the previous day" do 
+      page.find('h1').should have_selector('a', text: '<<')
+    end
+
+    it "displays a link to the next day" do 
+      page.find('h1').should have_selector('a', text: '>>')
+    end
 	end
 end
