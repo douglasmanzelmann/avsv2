2337570ee25a83919a5040fbc86eb8f68724aefc
douglas <dougamanzelmann@gmail.com>
2012-10-12 14:43:41 -0400

    Revert "tried rubymine... meh"
    This reverts commit c8e044e36a032d0f0a144adfd39e3f03867986ed.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
 describe Task do
-  before do
-    @task = Task.new(day: Date.today, room: "6W-002", begin_time: Time.now,
-                     end_time: 1.hour.from_now, gear: "LCD")
-  end
-
-  subject { @task }
-
-  it { should respond_to(:day) }
-  it { should respond_to(:room) }
-  it { should respond_to(:begin_time) }
-  it { should respond_to(:end_time) }
-  it { should respond_to(:gear) }
-  it { should respond_to(:preset) }
-  it { should respond_to(:setup) }
-  it { should respond_to(:strike) }
-  it { should respond_to(:notes) }
+  pending "add some examples to (or delete) #{__FILE__}"
 end
