c8e044e36a032d0f0a144adfd39e3f03867986ed
douglas <dougamanzelmann@gmail.com>
2012-10-12 14:43:22 -0400

    tried rubymine... meh

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @task = Task.new(day: Date.today, room: "6W-002", begin_time: Time.now,
+                     end_time: 1.hour.from_now, gear: "LCD")
+  end
+
+  subject { @task }
+
+  it { should respond_to(:day) }
+  it { should respond_to(:room) }
+  it { should respond_to(:begin_time) }
+  it { should respond_to(:end_time) }
+  it { should respond_to(:gear) }
+  it { should respond_to(:preset) }
+  it { should respond_to(:setup) }
+  it { should respond_to(:strike) }
+  it { should respond_to(:notes) }
 end
