8693ea19ad55170fd8cbdbc588df25cfd7a154de
douglas <dougamanzelmann@gmail.com>
2012-10-15 17:58:27 -0400

    task_pages_spec TaskController#Edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,7 @@ class TasksController < ApplicationController
   end
 
   def show
-
+    @task = Task.find(params[:id])
   end
 
   def update
--- spec/requests/tasks_pages_spec.rb
+++ spec/requests/tasks_pages_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe "Task Pages" do
 
+  let(:task) { FactoryGirl.create(:task) }
   subject { page }
-
-  describe "Creating a Task" do
+  
+  describe "Creates a Task" do
 
     let(:submit) { "Create task" }
     before { visit new_task_path }
 
-    describe "with valid information" do 
+    context "with valid information" do 
       before do 
         fill_in "Day",    with: Date.today
         fill_in "Room",   with: "6W-002"
@@ -21,6 +22,41 @@ describe "Task Pages" do
       it "creates a Task" do 
         expect { click_button submit }.to change(Task, :count).by(1)
       end
+    end
+  end
+
+  describe "Shows a Task" do 
+
+    context "with the corret information" do 
+      before { visit task_path(task) }
+
+      it { should have_selector('h1', text: "#{task.id}") }
+      it { should have_selector('li', text: "#{task.day}") }
+      it { should have_selector('li', text: "#{task.room}") }
+      it { should have_selector('li', text: "#{task.begin_time}") }
+      it { should have_selector('li', text: "#{task.end_time}") }
+      it { should have_selector('li', text: "#{task.gear}") }
+      it { should have_selector('li', text: "#{task.preset}") }
+      it { should have_selector('li', text: "#{task.setup}") }
+      it { should have_selector('li', text: "#{task.strike}") }
+      it { should have_selector('li', text: "#{task.notes}") }
+    end
+
+  end
+
+  describe "Edits a Task" do
+    let(:submit) { "Update task" }
+    before { visit edit_task_path(task) }
+
+    context "with valid information" do
+      let(:new_day) { Date.tomorrow }  
+    
+      before do 
+        fill_in "Day", with: new_day
+        click_button "Save changes"
+      end
+
+      it { should have_selector('li', text: new_day) }
 
     end
   end
